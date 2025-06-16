278db8e3fddc7f387d05df2af6fb4bc4cc90843a
Osman Bay <bayosman342@gmail.com>
2025-06-16 23:55:20 +0300

    appbarda'daki isim sorunu kesin düzeldi

--- src/App.jsx
+++ src/App.jsx
@@ -1,97 +1,123 @@
-import { Routes, Route, useLocation, useNavigate } from 'react-router-dom';
-import AppBar from './components/AppBar';
-import MainPageBanner from './components/Banner';
-import BlinkingScrollHint from './components/Scroll';
-import React, { useState, useEffect } from 'react';
-import ShowcaseArea from './components/Showcase';
-import Footer from './components/Footer';
-import HomePage from './pages/Home';
-import LearnPrice from './pages/LearnPrice';
-import SearchCar from './pages/SearchCar';
-import LearnMainPage from './components/LearnMainPage';
-import LearnLoginPage from './components/LearnLoginPage';
-import AuthDialog from './components/AuthDialog';
-import { AnimatePresence } from 'framer-motion';
-import api from './api/axios';
-import Kokpit from './pages/Kokpit';
-import QuickTransactions from './components/QuickTransactions';
-import MyAds from './components/MyAds';
-import FavMyAds from './components/FavMyAds';
-import MyMessages from './components/MyMessages';
-import ProfileAndSettings from './components/ProfileAndSettings';
-import YayindaOlanlar from './components/YayindaOlanlar';
-import YayindaOlmayanlar from './components/YayindaOlmayanlar';
-import { useSnackbar } from './context/SnackbarContext';
-import ResetPassword from './pages/ResetPassword';
-import CreateAdverts from './pages/CreateAdverts';
+import { Routes, Route, useLocation, useNavigate } from "react-router-dom";
+import AppBar from "./components/AppBar";
+import MainPageBanner from "./components/Banner";
+import BlinkingScrollHint from "./components/Scroll";
+import React, { useState, useEffect } from "react";
+import ShowcaseArea from "./components/Showcase";
+import Footer from "./components/Footer";
+import HomePage from "./pages/Home";
+import LearnPrice from "./pages/LearnPrice";
+import SearchCar from "./pages/SearchCar";
+import LearnMainPage from "./components/LearnMainPage";
+import LearnLoginPage from "./components/LearnLoginPage";
+import AuthDialog from "./components/AuthDialog";
+import { AnimatePresence } from "framer-motion";
+import api from "./api/axios";
+import Kokpit from "./pages/Kokpit";
+import QuickTransactions from "./components/QuickTransactions";
+import MyAds from "./components/MyAds";
+import FavMyAds from "./components/FavMyAds";
+import MyMessages from "./components/MyMessages";
+import ProfileAndSettings from "./components/ProfileAndSettings";
+import YayindaOlanlar from "./components/YayindaOlanlar";
+import YayindaOlmayanlar from "./components/YayindaOlmayanlar";
+import { useSnackbar } from "./context/SnackbarContext";
+import ResetPassword from "./pages/ResetPassword";
+import CreateAdverts from "./pages/CreateAdverts";
 
 export default function App() {
+  const [user, setUser] = useState(null);
   const [authOpen, setAuthOpen] = useState(false);
   const [authView, setAuthView] = useState("login");
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const location = useLocation();
   const { showSnackbar } = useSnackbar();
-  const hideFooterRoutes = ['/kokpit', '/fiyat-ogren', '/sifre-yenile', '/ilan-olustur'];
-  const shouldHideFooter = hideFooterRoutes.some(path => location.pathname.startsWith(path));
-  const navigate = useNavigate()
+  const hideFooterRoutes = [
+    "/kokpit",
+    "/fiyat-ogren",
+    "/sifre-yenile",
+    "/ilan-olustur",
+  ];
+  const shouldHideFooter = hideFooterRoutes.some((path) =>
+    location.pathname.startsWith(path)
+  );
+  const navigate = useNavigate();
 
   const handleOpenClick = (mode, id) => {
     setAuthView(mode);
     setAuthOpen(true);
     if (id !== "notLogin") {
-      showSnackbar('Bu işlemi yapmak için giriş yapınız.', 'error');
+      showSnackbar("Bu işlemi yapmak için giriş yapınız.", "error");
     }
   };
 
   useEffect(() => {
-    const token = localStorage.getItem('access_token');
+    const token = localStorage.getItem("access_token");
     setIsLoggedIn(!!token);
-  }, [location.pathname]); 
+  }, [location.pathname]);
 
   const handleLoginSuccess = () => {
     setIsLoggedIn(true);
     setAuthOpen(false);
     showSnackbar("Giriş başarılı!", "success");
+
+    const token = localStorage.getItem("access_token");
+    if (token) {
+      api
+        .get("/profile", {
+          headers: { Authorization: `Bearer ${token}` },
+        })
+        .then((res) => {
+          setUser(res.data);
+        });
+    }
   };
 
   const handleLogout = () => {
     setIsLoggedIn(false);
+    setUser(null)
     localStorage.removeItem("access_token");
     showSnackbar("Çıkış yapıldı.", "info");
-    navigate("/")
+    navigate("/");
   };
 
-  useEffect(() => {
-    const token = localStorage.getItem('access_token');
-    if (token) {
-      api.get('/profile', {
-        headers: {
-          Authorization: `Bearer ${token}`
-        }
-      })
-        .then(() => {
-          setIsLoggedIn(true);
-        })
-        .catch(() => {
-          setIsLoggedIn(false);
-          // localStorage.removeItem("access_token");
-        });
-    }
-  }, []);
-
-  
+  // useEffect(() => {
+  //   const token = localStorage.getItem("access_token");
+  //   if (token) {
+  //     api
+  //       .get("/profile", {
+  //         headers: {
+  //           Authorization: `Bearer ${token}`,
+  //         },
+  //       })
+  //       .then(() => {
+  //         setIsLoggedIn(true);
+  //       })
+  //       .catch(() => {
+  //         setIsLoggedIn(false);
+  //         // localStorage.removeItem("access_token");
+  //       });
+  //   }
+  // }, []);
 
   return (
     <>
-      <AppBar onOpenClick={handleOpenClick} isLoggedIn={isLoggedIn} onLogout={handleLogout} />
-      <AnimatePresence mode='wait'>
+      <AppBar
+        onOpenClick={handleOpenClick}
+        isLoggedIn={isLoggedIn}
+        onLogout={handleLogout}
+      />
+      <AnimatePresence mode="wait">
         <Routes>
-          <Route path='/' element={<HomePage />} />
-          <Route path='/arac-satin-al'>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/arac-satin-al">
             <Route index element={<SearchCar />} />
-            <Route path=':brand' element={<SearchCar />} />
+            <Route path=":brand" element={<SearchCar />} />
           </Route>
-          <Route path='/fiyat-ogren' element={<LearnPrice isLoggedIn={isLoggedIn} />}>
+          <Route
+            path="/fiyat-ogren"
+            element={<LearnPrice isLoggedIn={isLoggedIn} />}
+          >
             <Route index element={<LearnLoginPage />} />
             <Route path="marka" element={<LearnMainPage />} />
             <Route path="yil" element={<LearnMainPage />} />
@@ -104,18 +130,27 @@ export default function App() {
             <Route path="hasar" element={<LearnMainPage />} />
             <Route path="sonuc" element={<LearnMainPage />} />
           </Route>
-          <Route path='/kokpit' element={<Kokpit onLogout={handleLogout} />}>
+          <Route path="/kokpit" element={<Kokpit onLogout={handleLogout} />}>
             <Route index element={<QuickTransactions />} />
-            <Route path='ilanlarim' element={<MyAds />}>
+            <Route path="ilanlarim" element={<MyAds />}>
               <Route index element={<YayindaOlanlar />} />
-              <Route path='yayinda-olmayanlar' element={<YayindaOlmayanlar />} />
+              <Route
+                path="yayinda-olmayanlar"
+                element={<YayindaOlmayanlar />}
+              />
             </Route>
-            <Route path='fav-ilan' element={<FavMyAds />} />
-            <Route path='mesajlarim' element={<MyMessages />} />
-            <Route path='profil-ve-ayarlar' element={<ProfileAndSettings onOpenClick={handleOpenClick} />} />
+            <Route path="fav-ilan" element={<FavMyAds />} />
+            <Route path="mesajlarim" element={<MyMessages />} />
+            <Route
+              path="profil-ve-ayarlar"
+              element={<ProfileAndSettings onOpenClick={handleOpenClick} />}
+            />
           </Route>
-          <Route path='/sifre-yenile' element={<ResetPassword />} />
-          <Route path='/ilan-olustur' element={<CreateAdverts isLoggedIn={isLoggedIn} />}>
+          <Route path="/sifre-yenile" element={<ResetPassword />} />
+          <Route
+            path="/ilan-olustur"
+            element={<CreateAdverts isLoggedIn={isLoggedIn} />}
+          >
             <Route index path="marka" element={<CreateAdverts />} />
             <Route path="yil" element={<CreateAdverts />} />
             <Route path="model" element={<CreateAdverts />} />
